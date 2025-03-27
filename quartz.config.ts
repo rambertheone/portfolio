16c93033989b70e36cd604cff5eadb588982d71c
rambertheone <tsafacknahor1@gmail.com>
2025-03-27 10:41:48 -0400

    Quartz sync: Mar 27, 2025, 10:41 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "localhost:8080",
+    baseUrl: "ramberthe.one",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
